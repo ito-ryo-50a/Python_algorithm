9e9f72afa4552b17affe2fc718c557b3226ff2c7
ito-ryo-50a <1210riytoo@gmail.com>
2024-06-19 09:28:31 +0900

    edit

---
+++ A - Sanitize Hands.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc357/tasks/abc357_a
+
+
+# リファクタリングしたコード(必須)
+
+
+
+# 解法・記述のポイント
+
+H = [int(a) for a in input().split()]
+
+# ACしたコード(必須)
+
+
---
+++ record/A/A - Welcome to AtCoder Land.py
@@ -0,0 +1,27 @@
+# https://atcoder.jp/contests/abc358/tasks/abc358_a
+
+
+# リファクタリングしたコード(必須)
+def check_strings(a, b):
+    return "Yes" if a == "AtCoder" and b == "Land" else "No"
+
+def main():
+    a, b = input().split()
+    result = check_strings(a, b)
+    print(result)
+
+if __name__ == "__main__":
+    main()
+
+
+# 解法・記述のポイント
+
+
+
+# ACしたコード(必須)
+a, b = input().split()
+if a=="AtCoder" and b=="Land":
+  print("Yes")
+else:
+  print("No")
+
